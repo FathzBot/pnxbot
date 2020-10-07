dca4463214d09df3a4db65b49fe4b488398d9dca
KaanPNX <46761389+KaanPNX@users.noreply.github.com>
2020-10-07 09:09:27 +0300

    Update sarıl.js

--- pnx/commands/animeler/sarıl.js
+++ pnx/commands/animeler/sarıl.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const config = require("../../config.json")
 
 module.exports = {
-    name: "öp",
+    name: "sarıl",
     category: "animeler",
     description: "s",
     run: async (bot, message, args) => {
